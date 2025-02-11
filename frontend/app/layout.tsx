b96de3d28d2185468f35ab68e1352d88e0ae01dc
SpencerMonger <spencer@mongertrading.com>
2025-02-10 17:05:51 -0700

    favicons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
